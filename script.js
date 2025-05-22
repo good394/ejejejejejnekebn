7aaf4740ab6f7aef20b0384610c51a51255258b4
good394 <67219010006@thonburi.ac.th>
2025-05-22 12:22:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log("ธเนศพล หูแก้ว")
+// console.log("ปวช.1 เลขที่4")
+// console.log("เทคโนโลยีสารสนเทศ")
+// console.log("ตลาดคลองขวาง")
+// console.log("0612618875")
+// console.log("67219010006@thonburi.ac.th")
+// console.log("แพ็คของ,รับต่อกันดั้ม")
+
+//alert("welcome 2 webApp")
+
+document.writeln("ผลรวมของ 10 ", 10+10)
